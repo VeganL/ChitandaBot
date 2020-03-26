e4dbac9b0c51d1fe9dd8e38bdd0d6506dd45c4d6
Vegan Lroy <vl357@drexel.edu>
2020-03-25 22:26:41 -0400

    20200325 2225 Update: Supports more osu! bots, probability changed from 40% to 20%

--- bot.py
+++ bot.py
@@ -12,19 +12,18 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    elif ((message.author.id == 486848646344736770) or (message.author.id == 172002275412279296) or (message.author.id == 289066747443675143)) and (random.randint(0,10) <= 2):
+        await message.channel.send('*We don’t know if trying our best will help, but we do know, that if we don’t try our best, it won’t help for sure!*')
 
     msg = message.content
     questionPresent = re.search(r"\b(([Hh][Oo]+[Ww]+)|([Ww][Hh]*[Aa]+[Tt]+)|([Ww][Hh]+[Yy]+))\b",msg)
     uwu = re.search(r"\b([Uu]+[Ww]+[Uu]+)\b",msg)
     angry = re.search(r"\b([Rr][Ii]+[Pp]+ *([Ff][Cc]+)?|[Ff]+[Uu]+[Cc]*[Kk]* *([Mm]+[Ee]+)?|[Ff][Rr][Uu][Ss][Tt][Rr][Aa][Tt][Ii]+[Nn]+[Gg]+)\b",msg)
-    tryingBest = re.search(r"^((&|w!)recent)|((&|o!)top)",msg)
     dontGetIt = re.search(r"\b(([Dd][Oo][Nn]'?[Tt] +(([Gg][Ee][Tt] +[Ii]+[Tt]+)|([Uu][Nn][Dd][Ee][Rr][Ss][Tt][Aa]+[Nn]+[Dd]+)))|([Nn][Oo] +[Ss][Ee][Nn][Ss][Ee]))\b",msg)
 
-    if angry or tryingBest or dontGetIt:
+    if angry or dontGetIt:
         if angry:
             await message.channel.send('*If you can never get angry at anything, that probably means you have nothing that you like, either.*')
-        elif tryingBest and (random.randint(0,10) <= 4):
-            await message.channel.send('*We don’t know if trying our best will help, but we do know, that if we don’t try our best, it won’t help for sure!*')
         elif dontGetIt:
             await message.channel.send('*I find myself hard to understand sometimes.*')
     elif questionPresent:
