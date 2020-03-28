8b6278dedab7dc76811b3cd88d572350b5659acd
VeganL <veganlroy@gmail.com>
2020-03-28 19:13:45 -0400

    20200328 1912 Update: Added flowabot support (private host), made bot support more elegant, added "samui" to cold RegEx search

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
-import discord, re, random
+import discord, re, random, json
 
 client = discord.Client()
 tokenFile = open("token.txt", "r")
 token = tokenFile.read().rstrip()
+
+botsFile = open("supportedBots.json", "r")
+supportedOsuBots = json.loads(botsFile.read.rstrip())
 
 @client.event
 async def on_ready():
@@ -12,7 +15,7 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    elif ((message.author.id == 486848646344736770) or (message.author.id == 172002275412279296) or (message.author.id == 289066747443675143)) and (random.randint(0,10) <= 2):
+    elif ((message.author.id in supportedOsuBots.values())) and (random.randint(0,10) <= 2):
         await message.channel.send('*We don’t know if trying our best will help, but we do know, that if we don’t try our best, it won’t help for sure!*')
         return
 
@@ -21,7 +24,7 @@ async def on_message(message):
     uwu = re.search(r"\b([Uu]+[Ww]+[Uu]+)\b",msg)
     angry = re.search(r"\b([Rr][Ii]+[Pp]+ *([Ff][Cc]+)?|[Ff]+[Uu]+[Cc]*[Kk]* *([Mm]+[Ee]+)?|[Ff][Rr][Uu][Ss][Tt][Rr][Aa][Tt][Ii]+[Nn]+[Gg]+)\b",msg)
     dontGetIt = re.search(r"\b(([Dd][Oo][Nn]'?[Tt] +(([Gg][Ee][Tt] +[Ii]+[Tt]+)|([Uu][Nn][Dd][Ee][Rr][Ss][Tt][Aa]+[Nn]+[Dd]+)))|([Nn][Oo] +[Ss][Ee][Nn][Ss][Ee]))\b",msg)
-    cold = re.search(r"\b[Cc]+[Oo]+[Ll]+[Dd]+\b",msg)
+    cold = re.search(r"\b([Cc]+[Oo]+[Ll]+[Dd]+)|([Ss]+[Aa]+[Mm]+[Uu]+[Ii]+)\b",msg)
 
     if angry or dontGetIt or cold:
         if angry:
