faff2bc238620d27241efc9c6b2f22cc9cedf70a
Vegan Lroy <vl357@drexel.edu>
2020-03-31 12:55:51 -0400

    20200331 1254 Update: ChitandaBot now detects Japanese characters (50% chance), and now says signature phrase on japanese equivalents to english prompt words

--- bot.py
+++ bot.py
@@ -26,6 +26,8 @@ async def on_message(message):
     angry = re.search(r"\b([Rr][Ii]+[Pp]+ *([Ff][Cc]+)?|[Ff]+[Uu]+[Cc]*[Kk]* *([Mm]+[Ee]+)?|[Ff][Rr][Uu][Ss][Tt][Rr][Aa][Tt][Ii]+[Nn]+[Gg]+)\b",msg)
     dontGetIt = re.search(r"\b(([Dd][Oo][Nn]'?[Tt] +(([Gg][Ee][Tt] +[Ii]+[Tt]+)|([Uu][Nn][Dd][Ee][Rr][Ss][Tt][Aa]+[Nn]+[Dd]+)))|([Nn][Oo] +[Ss][Ee][Nn][Ss][Ee]))\b",msg)
     cold = re.search(r"\b([Cc]+[Oo]+[Ll]+[Dd]+)|([Ss]+[Aa]+[Mm]+[Uu]+[Ii]+)\b",msg)
+    nani = re.search(r"\u4F55|\u3069\u3046|\b[Nn]+[Aa]+[Nn]+[Ii]*\b|\b[Dd]+[Oo]+[Uu]+\b",msg)
+    japanese = re.search(r"[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]+",msg)
 
     if angry or dontGetIt or cold:
         if angry:
@@ -34,9 +36,11 @@ async def on_message(message):
             await message.channel.send('*I find myself hard to understand sometimes.*')
         elif cold:
             await message.channel.send('*Iie, mou haru desu.* :relaxed:')
-    elif questionPresent:
+    elif questionPresent or nani:
         await message.channel.send('*Watashi, ki ni narimasu!* :astonished:')
     elif uwu:
         await message.channel.send(':flushed:')
+    elif japanese and (random.randint(0,10) <= 5):
+        await message.channel.send('*日本語喋れる？*')
 
 client.run(token)
